f2d408b22fa2a939878f5b6c5f06e49009d60ea7
Pedro Gutierres <78659993+pedrogutierresbr@users.noreply.github.com>
2024-09-28 20:19:50 -0300

    first commit

---
+++ internal/infra/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/pedrogutierresbr/pos-goexpert/clean_arch-desafio-pos-goexpert/internal/usecase"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CreateOrderUseCase usecase.CreateOrderUseCase
+}
